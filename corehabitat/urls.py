edd7b6a81d037889289446faae2e757a49b5ef24
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-03 14:52:04 -0800

    added listings app

--- corehabitat/urls.py
+++ corehabitat/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('listings/', inclue('listings.urls')),
     path('admin/', admin.site.urls),
 ]
