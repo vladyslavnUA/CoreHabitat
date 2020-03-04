edb065dc552b8491cff0fbf41d393fe66167e589
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-04 03:40:44 -0800

    organization

--- corehabitat/listings/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    template_name = 'templates/base.html'
-    return render(request, template_name)
-
-# Create your views here.
